e49999cdf661a58b17745f59df107381d09ef683
Charles Moss <8564397+czmoss@users.noreply.github.com>
2019-01-04 02:40:03 -0600

    go/sieve: replace with /doc/progs/sieve1.go from old Go tutorial
    As noted in the README next to the C# translation, this
    implementation is closer to the most natural translation of
    sieve.go into C#.

--- go/sieve/main.go
+++ go/sieve/main.go
@@ -6,33 +6,46 @@ package main
 
 import "fmt"
 
-// Send the sequence 2, 3, 4, ... to channel 'ch'.
-func generate(ch chan int) {
-	for i := 2; ; i++ {
-		ch <- i // Send 'i' to channel 'ch'.
-	}
-}
-
-// Copy the values from channel 'in' to channel 'out',
-// removing those divisible by 'prime'.
-func filter(in, out chan int, prime int) {
-	for {
-		i := <-in // Receive value of new variable 'i' from 'in'.
-		if i%prime != 0 {
-			out <- i // Send 'i' to channel 'out'.
+// Send the sequence 2, 3, 4, ... to returned channel 
+func generate() chan int {
+	ch := make(chan int)
+	go func() {
+		for i := 2; ; i++ {
+			ch <- i
 		}
-	}
+	}()
+	return ch
 }
 
-// The prime sieve: Daisy-chain filter processes together.
+// Filter out input values divisible by 'prime', send rest to returned channel
+func filter(in chan int, prime int) chan int {
+	out := make(chan int)
+	go func() {
+		for {
+			if i := <-in; i%prime != 0 {
+				out <- i
+			}
+		}
+	}()
+	return out
+}
+
+func sieve() chan int {
+	out := make(chan int)
+	go func() {
+		ch := generate()
+		for {
+			prime := <-ch
+			out <- prime
+			ch = filter(ch, prime)
+		}
+	}()
+	return out
+}
+
 func main() {
-	ch := make(chan int)       // Create a new channel.
-	go generate(ch)            // Start generate() as a goroutine.
+	primes := sieve()
 	for i := 0; i < 100; i++ { // Print the first hundred primes.
-		prime := <-ch
-		fmt.Println(prime)
-		ch1 := make(chan int)
-		go filter(ch, ch1, prime)
-		ch = ch1
+		fmt.Println(<-primes)
 	}
 }
